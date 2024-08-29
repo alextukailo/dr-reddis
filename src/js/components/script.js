caa1b925b6d4cc54c9a0fc97b870fcc801aef720
Elizaveta <lizatukailo@gmail.com>
2024-08-29 11:37:06 +0300

    feat: add handler for select element

--- src/js/components/script.js
+++ src/js/components/script.js
@@ -14,15 +14,46 @@ const Script = () => {
     function addMenuPadding() {
         window.onscroll = function() {
             let height = document.querySelector(".header__inner").offsetHeight;
-            let currentScrollPos = window.pageYOffset;
             if (window.screen.width < 1024) {
                 document.querySelector("main").style.paddingTop = height + "px";
             }
-            //prevScrollpos = currentScrollPos;
         }
     }
     addMenuPadding();
     window.dispatchEvent(new CustomEvent('scroll'))
 
+
+   const handleCustomSelect = () => {
+    const selects = document.querySelectorAll('.custom-select');
+    selects.forEach(select => {
+        const input = select.querySelector('input');
+        const selectHead = select.querySelector('.select-head');
+        const selectList = select.querySelector('.select-items');
+        selectHead.addEventListener('click', function(e){
+            e.target.classList.toggle('open');
+            selectList.classList.toggle('active');
+        });
+        selectList.addEventListener('click', function(e) {
+            if (e.target.closest('.select-item')) {
+                const currentSelect = e.target;
+                input.value = currentSelect.dataset.value;
+                selectHead.classList.remove('open');
+                selectList.classList.remove('active');
+                selectHead.textContent = currentSelect.textContent;
+            }
+        });
+    });
+    document.addEventListener('click', function(e) {
+        selects.forEach(select => {
+            const selectHead = select.querySelector('.select-head');
+            const selectList = select.querySelector('.select-items');
+            if (!e.target.closest('.custom-select')) {
+                selectHead.classList.remove('open');
+                selectList.classList.remove('active');
+            }
+        });
+    });
+   }
+   handleCustomSelect();
 }
 export default Script;
